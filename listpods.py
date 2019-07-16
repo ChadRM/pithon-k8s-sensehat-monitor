166080ede389e2e8171bde26cc29e99188092171
ChadRM <chad.r.miller@gmail.com>
2019-07-16 14:39:11 -0500

    Finished porting to Unicorn Hat and added some utils

--- listpods.py
+++ listpods.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
 from kubernetes import client, config
-from sense_hat import SenseHat
+import unicornhathd
 
-bright_red = (255,0,0)
-bright_green = (0,255,0)
-bright_blue = (0,0,255)
-system_pod_healthy = (64,0,64)
-workload_pod_healthy = (0,0,64)
+#bright_red = (255,0,0)
+#bright_green = (0,255,0)
+#bright_blue = (0,0,255)
+#system_pod_healthy = (64,0,64)
+#workload_pod_healthy = (0,0,64)
 
-sense = SenseHat()
-sense.clear(0,0,0)
-sense.set_rotation(180)
+unicornhathd.clear()
+unicornhathd.rotation(270)
 config.load_kube_config()
 nameCount = {}
 v1 = client.CoreV1Api()
@@ -21,11 +20,11 @@ j = 0
 for i in ret1.items:
 #    print("%s\t%s\t%s\t%s" % (i.status.conditions[3].type,i.status.conditions[3].status, i.metadata.name, i.status.addresses[0].address))
     if i.status.conditions[3].status == "True":
-        sense.set_pixel(j,0,bright_green)
+        unicornhathd.set_pixel(j,0,0,255,0)
     elif i.status.conditions[3].status == "Unknown":
-        sense.set_pixel(j,0,bright_red)
+        unicornhathd.set_pixel(j,0,255,0,0)
     else:
-        sense.set_pixel(j,0,255,255,0)
+        unicornhathd.set_pixel(j,0,128,128,0)
     nameCount[i.status.addresses[0].address] = [0,j]
     j = j + 1
 
@@ -35,17 +34,18 @@ for i in ret2.items:
 #    print("%s\t%s\t%s" % (i.status.host_ip, i.metadata.namespace, i.metadata.name))
     if nameCount.has_key(i.status.host_ip):
       nameCount[i.status.host_ip][0] = nameCount[i.status.host_ip][0] + 1
-#      if nameCount[i.status.host_ip][0] > 7:
-#        nameCount[i.status.host_ip][0] = 7
+      if nameCount[i.status.host_ip][0] > 15:
+        nameCount[i.status.host_ip][0] = 15
       if i.metadata.namespace == "default":
-        sense.set_pixel(nameCount[i.status.host_ip][1],nameCount[i.status.host_ip][0],workload_pod_healthy)
+        unicornhathd.set_pixel(nameCount[i.status.host_ip][1],nameCount[i.status.host_ip][0],0,0,128)
 #        print("Set blue...(%s,%s)" % (nameCount[i.status.host_ip][1],nameCount[i.status.host_ip][0]))
       elif i.metadata.namespace == "kube-system":
-        sense.set_pixel(nameCount[i.status.host_ip][1],nameCount[i.status.host_ip][0],system_pod_healthy)
+        unicornhathd.set_pixel(nameCount[i.status.host_ip][1],nameCount[i.status.host_ip][0],64,0,64)
 #        print("Set purple...(%s,%s)" % (nameCount[i.status.host_ip][1],nameCount[i.status.host_ip][0]))
       else:
-        sense.set_pixel(nameCount[i.status.host_ip][1],nameCount[i.status.host_ip][0],16,16,16)
+        unicornhathd.set_pixel(nameCount[i.status.host_ip][1],nameCount[i.status.host_ip][0],16,16,16)
 #        print("Set grey...(%s,%s)" % (nameCount[i.status.host_ip][1],nameCount[i.status.host_ip][0]))
     else:
-      nameCount[i.status.host_ip] = (1,7)
+      nameCount[i.status.host_ip] = (1,15)
 
+unicornhathd.show()
